f8a7a03ddafb068d11b17a53456a33974e1aef10
Travis Swiers <tswiers@us.ci.org>
2023-09-06 20:48:12 -0600

    feat: tile dir and aesthetic prefix as config

--- src/windowConfig.h
+++ src/windowConfig.h
@@ -1,9 +1,11 @@
 #ifndef WINDOWCONFIG_H
 #define WINDOWCONFIG_H
 
-const int SCALE_X = 6;
-const int SCALE_Y= 6;
+const int SCALE_X = 3;
+const int SCALE_Y= 3;
 const int WINDOW_TILES_WIDE = 16;
 const int WINDOW_TILES_HIGH = 15;
+const std::string TILE_DIRECTORY = "src/game/assets/16x16Tiles";
+const std::string AESTHETIC_PREFIX = "dw";
 
 #endif
